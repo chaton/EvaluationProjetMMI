acd121d8e36456af72752a492bfe79725f021b7d
Pascal Chatonnay <pascal.chatonnay@univ-fcomte.fr>
2023-05-30 14:39:01 +0200

    Fin du test de fonctionnement de la connexion
    blop

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "localhost";
-$username = "";
-$password = "";
+$username = "root";
+$password = "root";
 $dbname = "EvaluationProjetMMI";
 ?>
--- testDeFonctionnement.php
+++ testDeFonctionnement.php
@@ -1,23 +1,44 @@
 <?php
 // Inclure le fichier de configuration
-require_once('config.php');
-print('Je suis passé par la </br>');
+require_once('connexion.php');
+
 // Requête de test
 $sql = "SELECT * FROM Etudiant";
 print('La requete est : '.$sql.'</br>');
+
 $result = $conn->query($sql) or die($conn->error);
 print('Le résultat est : '.$result->num_rows.'</br>');
 
+print('<h1> Affichage des données : </h1>');
 if ($result->num_rows > 0) {
+  // Création du tableau
+  echo "<table>";
+  
+  // En-têtes de colonne
+  echo "<tr>";
+  // on extrait les noms des colonnes
+  foreach ($row = $result->fetch_assoc() as $columnName => $value) {
+    echo "<th>$columnName</th>";
+  }
+  echo "</tr>";
+  // on reutilise la ligne précedente pour en extraire les données
+  echo "<tr>";
+    foreach ($row as $value) {
+    echo "<td>$value</td>";
+  }
+  echo "</tr>";
+
+
   // Affichage des données
   while ($row = $result->fetch_assoc()) {
-    echo "ID: " . $row["IdEtudiant_Etudiant"] . "<br>";
-    echo "Numéro d'étudiant: " . $row["NumeroEtudiant_Etudiant"] . "<br>";
-    echo "Nom: " . $row["Nom_Etudiant"] . "<br>";
-    echo "Prénom: " . $row["Prenom_Etudiant"] . "<br>";
-    echo "Date de naissance: " . $row["DateNaiss_Etudiant"] . "<br>";
-    echo "<br>";
+    echo "<tr>";
+    foreach ($row as $value) {
+      echo "<td>$value</td>";
+    }
+    echo "</tr>";
   }
+  
+  echo "</table>";
 } else {
   echo "Aucun résultat trouvé.";
 }
